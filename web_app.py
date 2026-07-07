e5ae7d005c5cb87f3f62db05cd52bc9306dfd12a
ravitleekha50-netizen <ravitleekha50@gmail.com>
2026-07-07 11:21:40 +0530

    Add deployment files

--- web_app.py
+++ web_app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify, Response
 from auditor.url_scanner import HeaderAuditor
 from auditor.ssl_checker import SSLChecker
@@ -79,4 +80,5 @@ def download_report():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=False, port=port)
